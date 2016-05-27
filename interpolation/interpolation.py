354308bcc8380b91747110547601802109eb7456
aybulatf <aybulat.f@gmail.com>
2016-05-27 16:53:22 +0200

    add inverse distance interpoation, fix the gausian grid

--- interpolation/interpolation.py
+++ interpolation/interpolation.py
@@ -136,5 +136,39 @@ class Interpolation:
         xv, yv = np.meshgrid(X_grid_x, X_grid_y)
         
         X_grid = np.dstack(( xv.flatten(), yv.flatten()))[0]
-        grid = np.reshape(gp.predict(X_grid, eval_MSE=False, batch_size=None), (nx, ny))
+        grid = np.reshape(gp.predict(X_grid, eval_MSE=False, batch_size=None), (ny, nx))
         return grid
+        
+    def inverse_distance_weighting(nx, ny, X, y, x_min, y_min, dx, dy, self):
+        """
+        Inverse-distance weighting interpolation method
+        """
+        power,smoothing = 5, 0
+        xv = [i[0] for i in X]
+        yv = [i[1] for i in X]    
+        grid = np.zeros((ny,nx))
+        for i in range(nx):
+            for j in range(ny):  
+                grid[j][i] = self.pointValue((x_min + dx/2)+dx*i,(y_min + dy/2)+dy*j,power,smoothing,xv,yv,y)  
+        return grid       
+        
+    def pointValue(x,y,power,smoothing,xv,yv,values):
+        """ This function is used inside the inverse_distance_weighting method. """
+        from math import pow  
+        from math import sqrt
+        
+        nominator=0  
+        denominator=0  
+        for i in range(0,len(values)):  
+            dist = sqrt((x-xv[i])*(x-xv[i])+(y-yv[i])*(y-yv[i])+smoothing*smoothing)
+            #If the point is really close to one of the data points, return the data point value to avoid singularities  
+            if(dist<0.0000000001):  
+                return values[i]
+            nominator=nominator+(values[i]/pow(dist,power))  
+            denominator=denominator+(1/pow(dist,power))  
+        #Return NODATA if the denominator is zero  
+        if denominator > 0:  
+            value = nominator/denominator  
+        else: 
+            value = -9999
+        return value  
