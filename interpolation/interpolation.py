a2b91e6598d4f3605fd1198b6a1c715179e9e2b1
aybulatf <aybulat.f@gmail.com>
2016-06-03 15:18:02 +0200

    fixing IDW interpolation method and adding test json file

--- interpolation/interpolation.py
+++ interpolation/interpolation.py
@@ -104,6 +104,8 @@ class Interpolation:
                     self._output = self.mean(self._nX, self._nY, self._Y)
                 elif self._method == 'gaussian':
                     self._output = self.gaussian_process(self._nX, self._nY, self._X, self._Y, self._xMin, self._yMin, self._dX, self._dY)
+                elif self._method == 'IDW':
+                    self._output = self.inverse_distance_weighting(self._nX, self._nY, self._X, self._Y, self._xMin, self._yMin, self._dX, self._dY)
                 else:
                     self._error = True
                     self._error_message = 'Method %s is not supported' % self._method
@@ -143,7 +145,7 @@ class Interpolation:
 
     @staticmethod
     def render(method, output):
-        if (method == 'kriging') or (method == 'mean') or (method == 'gaussian'):
+        if (method == 'kriging') or (method == 'mean') or (method == 'gaussian') or (method == 'IDW'):
             result = {"raster": output, "method": method}
             return demjson.encode(result)
 
@@ -187,37 +189,37 @@ class Interpolation:
         return grid
 
     @staticmethod
-    def inverse_distance_weighting(nx, ny, X, y, x_min, y_min, dx, dy, self):
+    def inverse_distance_weighting(nx, ny, X, y, x_min, y_min, dx, dy):
         """
         Inverse-distance weighting interpolation method
         """
+
+        def pointValue(x, y, power, smoothing, xv, yv, values):
+            """ This function is used inside the inverse_distance_weighting method. """
+            from math import pow  
+            from math import sqrt
+            
+            nominator=0  
+            denominator=0  
+            for i in range(0,len(values)):  
+                dist = sqrt((x-xv[i])*(x-xv[i])+(y-yv[i])*(y-yv[i])+smoothing*smoothing)
+                #If the point is really close to one of the data points, return the data point value to avoid singularities  
+                if(dist<0.0000000001):  
+                    return values[i]
+                nominator=nominator+(values[i]/pow(dist,power))  
+                denominator=denominator+(1/pow(dist,power))  
+            #Return NODATA if the denominator is zero  
+            if denominator > 0:  
+                value = nominator/denominator  
+            else: 
+                value = -9999
+            return value
+            
         power,smoothing = 5, 0
         xv = [i[0] for i in X]
         yv = [i[1] for i in X]    
         grid = np.zeros((ny,nx))
         for i in range(nx):
             for j in range(ny):  
-                grid[j][i] = self.pointValue((x_min + dx/2)+dx*i, (y_min + dy/2)+dy*j, power, smoothing, xv, yv, y)
+                grid[j][i] = pointValue((x_min + dx/2)+dx*i, (y_min + dy/2)+dy*j, power, smoothing, xv, yv, y)
         return grid
-
-    @staticmethod
-    def pointValue(x, y, power, smoothing, xv, yv, values):
-        """ This function is used inside the inverse_distance_weighting method. """
-        from math import pow  
-        from math import sqrt
-        
-        nominator=0  
-        denominator=0  
-        for i in range(0,len(values)):  
-            dist = sqrt((x-xv[i])*(x-xv[i])+(y-yv[i])*(y-yv[i])+smoothing*smoothing)
-            #If the point is really close to one of the data points, return the data point value to avoid singularities  
-            if(dist<0.0000000001):  
-                return values[i]
-            nominator=nominator+(values[i]/pow(dist,power))  
-            denominator=denominator+(1/pow(dist,power))  
-        #Return NODATA if the denominator is zero  
-        if denominator > 0:  
-            value = nominator/denominator  
-        else: 
-            value = -9999
-        return value  
