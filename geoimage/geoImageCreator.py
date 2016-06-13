344453fb8ae77b728102923ad53f1b45f3345592
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-06-13 22:07:50 +0200

    Change names and add execution-script

--- geoimage/geoImageCreator.py
+++ geoimage/geoImageCreator.py
@@ -6,11 +6,13 @@ Created on Mon Jun 13 15:33:06 2016
 """
 
 import sys
-from make_image import RasterImage
+from geoImage import RasterImage
 
 input_file_name = sys.argv[1]
+output_file_name = sys.argv[2]
 
 r = RasterImage()
 r.from_file(input_file_name)
+r._name = output_file_name
 newFile = r.makeImage()
 print newFile
