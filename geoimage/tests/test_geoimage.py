39365a922485073b6cd9a931ad495327e138557f
aybulatf <aybulat.f@gmail.com>
2016-06-14 15:55:55 +0200

    test_geoimage

--- geoimage/tests/test_geoimage.py
+++ geoimage/tests/test_geoimage.py
@@ -1,26 +1,37 @@
 import unittest
 from geoImage import RasterImage
 import os
+from scipy import misc
+
 
 class Test_geoimage(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls._r = RasterImage()
-    @classmethod
-    def tearDownClass(cls):
-        cls._r =None
 
-    def test_if_file_created(self):
+    def setUp(self):
         sample_data_name = 'example_data.json'
         name = 'newimage'
         workspace = 'images'
-        fileName = os.path.join(workspace, name)
-        if os.path.exists(fileName):
-            os.remove(fileName)
-        self._r.from_file(sample_data_name)
-        self._r.setOutputFileName(name)
-        result = self._r.makeImage()
-        self.assertTrue(os.path.exists(fileName + '.png'))
+        self.r = RasterImage()
+        self.fileName = os.path.join(workspace, name + '.png')
+        if os.path.exists(self.fileName):
+            os.remove(self.fileName)
+        self.r.from_file(sample_data_name)
+        self.r.setOutputFileName(name)
+        self.r.makeImage()
+
+    def tearDown(self):
+        if os.path.exists(self.fileName):
+            os.remove(self.fileName)
+        self.r = None
+        self.fileName = None
+
+    def test_if_file_created(self):
+        self.assertTrue(os.path.exists(self.fileName))
+
+    def test_image_properities(self):
+        image = misc.imread(self.fileName)
+        self.assertEqual(len(image.shape), 3)
+        self.assertEqual(image.shape[2], 4)
+        self.assertEqual(image.dtype, 'uint8')
 
 if __name__ == '__main__':
     unittest.main()
