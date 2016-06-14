388644855ddb75166edaf23dd8d003abf80a76f7
aybulatf <aybulat.f@gmail.com>
2016-06-14 16:18:49 +0200

    test_intersector

--- modflow/tests/test_intersector.py
+++ modflow/tests/test_intersector.py
@@ -21,19 +21,20 @@ class Test_line_area_intersectr(unittest.TestCase):
         nx, ny = 11, 11
         xmin, xmax, ymin, ymax = 0., 400., 0., 400.
         line = [[0., 0.], [0., 300.], [400., 300.],
-                 [400., 0.], [0., 0.]]
+                [400., 0.], [0., 0.]]
 
-        line_cols, line_rows = intersector.line_area_intersect(line, xmax, xmin, ymax,
-                                                   ymin, nx, ny)
+        line_cols, line_rows = intersector.line_area_intersect(line, xmax,
+                                                               xmin, ymax,
+                                                               ymin, nx, ny)
 
-        control_line_cols = [0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,9,10,10,10,
+        _control_line_cols = [0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,9,10,10,10,
                              10,10,10,10,10,10,9,8,7,6,5,4,3,2,1]
-        control_line_rows = [0,1,2,3,4,5,6,7,8,8,8,8,8,8,8,8,8,8,8,
+        _control_line_rows = [0,1,2,3,4,5,6,7,8,8,8,8,8,8,8,8,8,8,8,
                              7,6,5,4,3,2,1,0,0,0,0,0,0,0,0,0,0]
 
-
-        self.assertEqual(line_cols.tolist(), control_line_cols)
-        self.assertEqual(line_rows.tolist(), control_line_rows)
+        self.assertEqual(len(line_cols), len(line_rows))
+        self.assertEqual(line_cols.tolist(), _control_line_cols)
+        self.assertEqual(line_rows.tolist(), _control_line_rows)
 
 if __name__ == '__main__':
     unittest.main()
