524d5802410066e6939491a35d8444227588f5bc
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-12-19 18:36:10 +0100

    Add heder to submit data

--- flopy/InowasFlopy.py
+++ flopy/InowasFlopy.py
@@ -349,6 +349,7 @@ class InowasFlopy:
         data = urllib.parse.urlencode(values)
         data = data.encode('utf-8')
         req = urllib.request.Request(url, data)
+        req.add_header('X-AUTH-TOKEN', api_key)
         resp = urllib.request.urlopen(req)
         respData = resp.read()
         print(respData)
